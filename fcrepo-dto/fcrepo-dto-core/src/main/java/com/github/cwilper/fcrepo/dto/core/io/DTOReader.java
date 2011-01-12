20f2276462f91202905b9088b855d846ea1ddea4
Chris Wilper <cwilper@gmail.com>
2011-01-12 17:13:05 -0500

    adds javadocs for dtoreader/writer interfaces

--- fcrepo-dto/fcrepo-dto-core/src/main/java/com/github/cwilper/fcrepo/dto/core/io/DTOReader.java
+++ fcrepo-dto/fcrepo-dto-core/src/main/java/com/github/cwilper/fcrepo/dto/core/io/DTOReader.java
@@ -5,10 +5,26 @@ import com.github.cwilper.fcrepo.dto.core.FedoraObject;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Interface for reading a <code>FedoraObject</code> from a stream.
+ */
 public interface DTOReader {
 
+    /**
+     * Deserializes the given stream into a <code>FedoraObject</code>.
+     *
+     * @param source the stream to read from. It will be closed by the time
+     *        this method exits, regardless of success.
+     * @return a new <code>FedoraObject</code> based on the content of the
+     *         stream.
+     * @throws IOException if the stream cannot be deserialized for any reason.
+     */
     FedoraObject readObject(InputStream source) throws IOException;
 
+    /**
+     * Releases all resources associated with this reader.
+     * This can be safely called multiple times.
+     */
     void close();
     
 }
--- fcrepo-dto/fcrepo-dto-core/src/main/java/com/github/cwilper/fcrepo/dto/core/io/DTOWriter.java
+++ fcrepo-dto/fcrepo-dto-core/src/main/java/com/github/cwilper/fcrepo/dto/core/io/DTOWriter.java
@@ -5,9 +5,24 @@ import com.github.cwilper.fcrepo.dto.core.FedoraObject;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * Interface for writing a <code>FedoraObject</code> to a stream.
+ */
 public interface DTOWriter {
 
+    /**
+     * Serializes a <code>FedoraObject</code> to the given stream.
+     *
+     * @param obj the <code>FedoraObject</code> to serialize.
+     * @param sink the stream to write to. It will not be automatically
+     *        closed by this method, regardless of success.
+     * @throws IOException if the stream cannot be serialized for any reason.
+     */
     void writeObject(FedoraObject obj, OutputStream sink) throws IOException;
 
+    /**
+     * Releases all resources associated with this writer.
+     * This can be safely called multiple times.
+     */
     void close();
 }
