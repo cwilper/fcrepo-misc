95cc77389d543deea529b5240314f6b5e46089a1
Chris Wilper <cwilper@gmail.com>
2011-04-20 11:44:26 -0400

    remove fcrepo-flexport

--- fcrepo-flexport/src/main/bin/fcrepo-flexport.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-echo This doesn't work yet.
--- fcrepo-flexport/src/main/java/com/github/cwilper/fcrepo/flexport/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * A command-line utility and supporting classes for exporting selected
- * objects and datastream content from Fedora repositories.
- */
-package com.github.cwilper.fcrepo.flexport;
