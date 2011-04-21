af75aec4444df879946e3988b7d4222583a0b1a4
Chris Wilper <cwilper@gmail.com>
2011-04-21 17:18:52 -0400

    better auto-gen'd wadl and support of json or xml responses by extension

---
+++ fcrepo-cloudsync/fcrepo-cloudsync-service/src/main/java/com/github/cwilper/fcrepo/cloudsync/service/CloudSyncServiceImpl.java
@@ -0,0 +1,6 @@
+package com.github.cwilper.fcrepo.cloudsync.service;
+
+import com.github.cwilper.fcrepo.cloudsync.api.CloudSyncService;
+
+public class CloudSyncServiceImpl implements CloudSyncService {
+}
--- fcrepo-cloudsync/fcrepo-cloudsync-service/src/main/java/com/github/cwilper/fcrepo/cloudsync/service/rest/TaskResource.java
+++ fcrepo-cloudsync/fcrepo-cloudsync-service/src/main/java/com/github/cwilper/fcrepo/cloudsync/service/rest/TaskResource.java
@@ -1,24 +1,36 @@
 package com.github.cwilper.fcrepo.cloudsync.service.rest;
 
+import com.github.cwilper.fcrepo.cloudsync.api.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-@Path("/tasks/")
+@Path("tasks")
 public class TaskResource {
 
     private static final Logger logger =
             LoggerFactory.getLogger(TaskResource.class);
 
     @GET
-    @Path("/")
     public Response list() {
         logger.info("Got request to list");
         Response r = Response.ok("Here's the list of tasks").build();
         return r;
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Task show(@PathParam("id") String id) {
+        logger.info("Got request to show");
+        Task task = new Task();
+        task.setId(id);
+        return task;
     }
 
     /*
